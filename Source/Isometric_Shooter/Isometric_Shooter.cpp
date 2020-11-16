0e917c540b7cd08658359c11a66a984041a38448
DaniloVanegas <DaniloVanegas>
2020-11-15 23:51:30 -0500

    Project Setup

---
+++ Source/Isometric_Shooter/Isometric_Shooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Isometric_Shooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Isometric_Shooter, "Isometric_Shooter" );
